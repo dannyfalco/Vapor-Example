685abab70037ebb759f96e6ea7219160664e4e2e
Falco Danny (CORP) <danny.falco@adp.com>
2017-05-31 23:13:18 -0400

    added public and resource folders

--- Friends/Sources/main.swift
+++ Friends/Sources/main.swift
@@ -1,11 +1,12 @@
 import Vapor
 import VaporPostgreSQL
+import HTTP
 
 
 //let config = try Config()
 //try config.setup()
 
-//let drop = try Droplet(config)
+//let drop = try  Droplet(config)
 let drop = Droplet()
 drop.preparations.append(Friend.self)
 //try drop.setup()
@@ -17,7 +18,16 @@ do {
 }
 
 drop.get { req in
-    return try drop.view.make("welcome", "title")
+  let lang = req.headers["Accept-Language"]?.string ?? "en"
+  return try drop.view.make("welcome", [
+        "message": drop.localization[req.lang, "welcome", "title"]
+    ])
+    //return try drop.view.make("welcome", "title")
+}
+
+drop.get("helloworld") { req in
+  //return try drop.view.make("hello")
+  return try drop.view.make("hello", ["greeting": "World"])
 }
 
 drop.get("friends") { req in
@@ -26,7 +36,7 @@ drop.get("friends") { req in
     return try JSON(node: friendsDictionary)
 }
 
-drop.get("friends", Int.self) {req, userID in
+drop.get("friends", Int.self) { req, userID in
   guard let friend = try Friend.find(userID) else {
     throw Abort.notFound
   }
@@ -37,6 +47,17 @@ drop.post("friend") { req in
     var friend = try Friend(node: req.json)
     try friend.save()
     return try friend.makeJSON()
+}
+
+drop.delete("friends", Int.self) { req, userID in
+  guard let friend = try Friend.find(userID) else {
+      throw Abort.notFound
+  }
+
+  try friend.delete()
+  return Response(status: .ok,
+    headers: ["Content-Type": "text/plain"], body: "Delete successful.")
+
 }
 
 drop.resource("posts", PostController())
